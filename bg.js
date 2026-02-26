16f7f5199668a9077d39765289a99b1d932a2162
jm_1211 <34554741+chimneysandsmokestacks@users.noreply.github.com>
2026-02-25 23:39:46 -0500

    Update radial gradient color stops in bg.js

--- bg.js
+++ bg.js
@@ -89,7 +89,8 @@
     var r  = Math.min(W, H) * 0.075;
     var gr = ctx.createRadialGradient(sx, sy, 0, sx, sy, r);
     gr.addColorStop(0.00, 'rgba(255, 192, 129, 0.9)'); /* solid centre      */
-    gr.addColorStop(0.60, 'rgba(255, 150, 45, 0.72)'); /* solid centre      */
+    gr.addColorStop(0.60, 'rgba(255, 192, 129, 0.9)'); /* solid centre      */
+   // gr.addColorStop(0.00, 'rgba(255, 150, 45, 0.72)'); /* solid centre      */
     gr.addColorStop(0.70, 'rgba(255, 145, 40, 0.72)'); /* holds solid to 70%*/
     gr.addColorStop(0.88, 'rgba(240, 110, 28, 0.34)'); /* sharp mist drop   */
     gr.addColorStop(1.00, 'rgba(210,  80, 15, 0)');    /* fully gone at edge*/
